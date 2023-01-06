14c1366bdebba2122577b101b9c18cff73dd3780
Sidnei Junior <sidneijuniorx@icloud.com>
2023-01-06 20:53:29 -0300

    Update extrair_seq.py

--- extrair_seq.py
+++ extrair_seq.py
@@ -1,5 +1,6 @@
-IDPro = open('Aspnid_stids.txt','r')
-SeqPro = open('Aspnid_proteins.fa','r')
+IDPro = open('aaaaa.txt','r')
+SeqPro = open('aspnid_pf00083_minuspenchr.fasta','r')
+string_output = 'saida.fasta'
 
 ID = IDPro.readlines()
 Seq = SeqPro.readlines()
@@ -9,21 +10,29 @@ SeqPro.close()
 i = 0
 j = 0
 
+#print(len(ID))
+#print(len(Seq))
+
 for i in range(len(ID)):
-    #print(line)
+    #print(str(i)+' = '+ID[i])
     for j in range(len(Seq)):
         aux = j
         for char in ' \n':  
             line = ID[i].replace(char,'')
             if '>'+line+'\n' in Seq[aux]:
-                output = open('aspnid_sugar_transporters.fasta','a')
+                output = open(string_output,'a')
                 output.write(Seq[aux])
                 output.close()
                 aux = aux+1
                 while not '>' in Seq[aux]:
-                    output = open('aspnid_sugar_transporters.fasta','a')
+                    output = open(string_output,'a')
                     output.write(Seq[aux])
                     output.close()
                     aux = aux+1
+                    if aux == len(Seq):
+                        output = open(string_output,'a')
+                        output.write('\n')
+                        output.close()
+                        break
                 break
-        aux = j
+            aux = j
